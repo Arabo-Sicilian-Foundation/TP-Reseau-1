4614af6d49a0263f6a616586bc5624d3cde70b81
Hugo <ihfisk42@gmail.com>
2018-10-20 17:49:16 -0400

    Add files via upload

--- MAC.cpp
+++ MAC.cpp
@@ -1,4 +1,6 @@
 #include "MAC.h"
+#include "Hash.h"
+
 #define BBITS 8
 
 
@@ -14,35 +16,37 @@ MAC::~MAC()
 std::string MAC::hmac(std::string k, std::string m)
 {
 	std::string zeros;
-	std::string s_i;
 	std::string s_0;
-
-	// c'est surement inutile et doit y avoir plus simple
-	std::bitset<BBITS * 8> kplus;
-	std::bitset<BBITS * 8> kBin;
-	std::bitset<8> ipad = std::bitset<8>(00110110);
-	std::bitset<8> opad = std::bitset<8>(01011100);
-
-	while (k.length() <  BBITS /* 64 bits */)
-	{
-		// on rajoute des 0 a gauche de k pour obtenir k+
-		k.insert(0, 'NUL');
-	}
+	std::string s_i = "00000000";
+	Hash* hache = new Hash();
 
 	// s_i = xor(kplus,00110110); 
-	//ca marche pas du coup la
-	kBin = std::bitset<BBITS * 8>(k);
-	kplus = kBin ^ ipad;
+	for (int i = 0; i < k.size(); i++)
+	{
+		char tmp = k[i] ^ '6';
+		s_i[i] = tmp;
+	}
 
 	// s_i = concat(s_i,m);
+	s_i += m;
 
 	// s_i = hachage(s_i);
+	hache->fonctionHachage(s_i);
+	s_i = hache->getMessageHacher();
 
 	// s_0 = xor(kplus,01011100);
+	for (int i = 0; i < k.size(); i++)
+	{
+		char tmp = k[i] ^ '6';
+		s_0[i] = tmp;
+	}
 
 	//s_0 = concat(s_0, s_i);
+	s_0 += s_i;
 
 	// s_0 = hachage(s_0);
+	hache->fonctionHachage(s_0);
+	s_0 = hache->getMessageHacher();
 
 	return s_0;
 }
