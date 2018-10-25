660d59b8280217eccbca1e4e90390ac69ab98333
Sicilien <32875283+Sicilien@users.noreply.github.com>
2018-10-25 16:47:14 -0400

    Add files via upload

--- Counter.cpp
+++ Counter.cpp
@@ -7,6 +7,7 @@ Counter::Counter()
 	decoupageBlocs = new string[20];
 	cleChiffrees = new string[20];
 	texteChiffre = new string[20];
+	texteDechiffre = new string[20];
 }
 
 
@@ -21,11 +22,11 @@ void Counter::decouperBlocs(string message)
 	int i, j, k;
 	int combler = message.length() % 8;
 
-	// Si la taille du message n'est pas un multiple de 8, on rajoute des '0' au message
+	// Si la taille du message n'est pas un multiple de 8, on rajoute des '#' au message
 	if (combler != 0)
 	{
 		for (i = 8;i > combler;i--)
-			message.push_back('0');
+			message.push_back('#');
 	}
 
 	// Réparti le message dans les blocs
@@ -83,6 +84,28 @@ void Counter::counterMode(string message, string cle)
 			texteChiffre[j].push_back(temp);
 		}
 		cout << texteChiffre[j] << " ";
+	}
+	cout << endl;
+}
+
+void Counter::dechiffrerBlocs()
+{
+	int i, j;
+
+	cout << "Blocs dechiffres: ";
+
+	// Déchiffre les blocs avec un ou exclusif entre le bloc de message chiffré et le bloc de la clé chiffrée
+	for (j = 0;j < nombreBlocs;j++)
+	{
+		for (i = 0; i < 8; i++)
+		{
+			char temp = texteChiffre[j][i] ^ cleChiffrees[j][i];
+
+			// On supprime les '#' qui ont été ajouté lors de la création des blocs
+			if(temp != '#')
+				texteDechiffre[j].push_back(temp);
+		}
+		cout << texteDechiffre[j] << " ";
 	}
 	cout << endl;
 }
--- Counter.h
+++ Counter.h
@@ -13,15 +13,18 @@ public:
 	~Counter();
 	string* getDecoupageBlocs() const { return decoupageBlocs; };
 	string* getTexteChiffre() const { return texteChiffre; };
+	string* getTexteDechiffre() const { return texteDechiffre; };
 	int getNombreBlocs() const { return nombreBlocs; };
 	void decouperBlocs(string message);
 	void chiffrementCle(string cle);
 	void counterMode(string message, string cle);
+	void dechiffrerBlocs();
 private:
 	int nombreBlocs;
 	string* decoupageBlocs;
 	string* cleChiffrees;
 	string* texteChiffre;
+	string* texteDechiffre;
 };
 
 #endif
