5ec38d0530717a697cfa5774136cc75caf86197e
Sicilien <32875283+Sicilien@users.noreply.github.com>
2018-10-25 16:53:07 -0400

    Add files via upload

--- Vigenere.cpp
+++ Vigenere.cpp
@@ -13,6 +13,8 @@ Vigenere::~Vigenere()
 {
 }
 
+// Le cryptage et le décryptage se basent sur la table ascii, soit des codes ascii allant de 0 à 127
+// Crypte le message
 void Vigenere::crypter(string cle)
 {
 	int tailleCle = cle.length();
@@ -25,13 +27,14 @@ void Vigenere::crypter(string cle)
 	}
 }
 
+// Décrypte le message
 void Vigenere::decrypter(string cle)
 {
 	int tailleCle = cle.length();
 	int tailleMessage = messageCode.length();
 	int i;
 
-	messageDecode = messageCode;
+	messageDecode = messageCode; // Permet seulement de créer un messageDecode de la bonne taille
 
 	for (i = 0;i < tailleMessage; i++)
 	{
